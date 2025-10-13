1212f3c37ac52bfbb255bddae649cef19d975c97
Kseniia Terenko <kseniia.terenko@gmail.com>
2025-10-13 16:21:30 +0200

    Add Postcss To Tsdown Config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from 'tsdown';
+import postcss from 'rollup-plugin-postcss';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import tailwind from '@tailwindcss/postcss';
 
-export default defineConfig([
-  {
-    entry: ['./src/index.ts'],
-    platform: 'neutral',
-    dts: true,
-  },
-])
+export default defineConfig({
+	entry: ['src/index.ts'],
+	dts: true,
+	outDir: 'dist',
+	plugins: [
+		nodeResolve({ extensions: ['.mjs', '.js', '.ts', '.tsx', '.css'] }),
+		postcss({
+			plugins: [tailwind()],
+			extract: 'index.css',
+			sourceMap: false,
+		}),
+	],
+});
