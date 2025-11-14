82bdf290b68cf52e90280b2aad6596d1c46ca490
Kseniia Terenko <kseniia.terenko@gmail.com>
2025-11-14 13:49:13 +0100

    #21 Move useEventListener hook with tests to separate folder

--- src/hooks/useEventListener/useEventListener.test.ts
+++ src/hooks/useEventListener/useEventListener.test.ts
@@ -83,3 +83,4 @@ describe('useEventListener', () => {
 		addEventListenerSpy.mockRestore();
 	});
 });
+
--- src/hooks/useEventListener/useEventListener.ts
+++ src/hooks/useEventListener/useEventListener.ts
@@ -65,3 +65,4 @@ export function useEventListener<K extends keyof HTMLElementEventMap>(
 		};
 	}, [eventName, element, options]);
 }
+
