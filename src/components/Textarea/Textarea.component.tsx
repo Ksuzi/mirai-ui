0697cb76966597184dec02a09885a459eebee8b4
Kseniia Terenko <kseniia.terenko@gmail.com>
2025-11-18 14:30:13 +0100

    #32 Refactor Label and Textarea components to remove unnecessary React.memo

--- src/components/Textarea/Textarea.component.tsx
+++ src/components/Textarea/Textarea.component.tsx
@@ -6,38 +6,28 @@ import { textareaVariants } from './Textarea.variants';
 
 import type { TextareaProps } from './Textarea.types';
 
-export const Textarea = React.memo(
-	React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-		(
-			{
-				variant = 'default',
-				state = 'default',
-				size = 'md',
-				resize = 'vertical',
-				fullWidth = true,
-				className,
-				...props
-			},
-			ref
-		) => {
-			return (
-				<textarea
-					ref={ref}
-					className={mergeClassNames(
-						textareaVariants({
-							variant,
-							state,
-							size,
-							resize,
-							fullWidth,
-						}),
-						className
-					)}
-					{...props}
-				/>
-			);
-		}
-	)
+export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+	(
+		{ variant = 'default', state = 'default', size = 'md', resize = 'vertical', fullWidth = true, className, ...props },
+		ref
+	) => {
+		return (
+			<textarea
+				ref={ref}
+				className={mergeClassNames(
+					textareaVariants({
+						variant,
+						state,
+						size,
+						resize,
+						fullWidth,
+					}),
+					className
+				)}
+				{...props}
+			/>
+		);
+	}
 );
 
 Textarea.displayName = 'Textarea';
